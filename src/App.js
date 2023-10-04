567792e4d6e8b8107e24ff6d2d9f58293dab745f
Jón Eðvarð Viðarsson <jon_eddi@hotmail.com>
2023-10-04 21:43:11 +0000

    OFFICIAL intial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Container, Grid } from '@mui/material';
+
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Resume from './pages/Resume/Resume';
+
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Container>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={4} lg={3} >
+          <Profile />
+        </Grid>
+        <Grid item xs>
+          <Header />
+            <Router>
+              <Routes>
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/" element={<Resume />} />
+              </Routes>
+            </Router>
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
